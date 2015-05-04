567a6394453a95e707bb2ea8a1508dec551e82e6
fdesimini <fdesimini@users.noreply.github.com>
2015-05-04 14:03:05 -0400

    Initial Commit

---
+++ BikeShareApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BikeShareApp
+//
+//  Created by Frank Desimini on 2015-05-04.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BikeShareApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BikeShareApp
+//
+//  Created by Frank Desimini on 2015-05-04.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
