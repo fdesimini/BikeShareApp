294023230e4cda7ec00cf099014bd5eadb9d2287
fdesimini <fdesimini@users.noreply.github.com>
2015-05-06 16:46:19 -0400

    created annotation class

---
+++ Annotation.h
@@ -0,0 +1,16 @@
+//
+//  Annotation.h
+//  BikeShareApp
+//
+//  Created by Frank Desimini on 2015-05-06.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface Annotation : NSObject <MKAnnotation>
+
+@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+
+@end
---
+++ Annotation.m
@@ -0,0 +1,13 @@
+//
+//  Annotation.m
+//  BikeShareApp
+//
+//  Created by Frank Desimini on 2015-05-06.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+#import "Annotation.h"
+
+@implementation Annotation
+
+@end
